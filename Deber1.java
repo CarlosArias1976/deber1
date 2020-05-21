fd613132d944e2d169fd4ef4cc4a4505ff110d09
CarlosArias1976 <carlosariasgallo@hotmail.com>
2020-05-20 22:03:21 -0500

    codigo para las caracteristicas de GitHub

---
+++ Deber1.java
@@ -0,0 +1,80 @@
+/*
+ * deber de programacion.
+ */
+package deber1;
+
+
+/**
+ *
+ * @author carlos
+ */
+
+import java.util.Scanner;
+
+
+public class Deber1 {
+   
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner entrada = new Scanner (System.in);
+         
+         System.out.println("ingresar caracteristica1");
+         String caracteristica1 = entrada.nextLine();
+         System.out.println("caracteristica 1: "+caracteristica1);
+         
+         System.out.println("ingresar caracteristica2");
+         String caracteristica2 = entrada.nextLine();
+         System.out.println("caracteristica 2: "+caracteristica2);
+         
+         System.out.println("ingresar caracteristica3");
+         String caracteristica3 = entrada.nextLine();
+         System.out.println("caracteristica 3: "+caracteristica3);
+         
+         System.out.println("ingresar caracteristica4");
+         String caracteristica4 = entrada.nextLine();
+         System.out.println("caracteristica 4: "+caracteristica4);
+         
+         System.out.println("ingresar caracteristica5");
+         String caracteristica5 = entrada.nextLine();
+         System.out.println("caracteristica 5: "+caracteristica5);
+         
+         System.out.println("ingresar caracteristica6");
+         String caracteristica6 = entrada.nextLine();
+         System.out.println("caracteristica 6: "+caracteristica6);
+         
+         System.out.println("ingresar caracteristica7");
+         String caracteristica7 = entrada.nextLine();
+         System.out.println("caracteristica 7: "+caracteristica7);
+         
+         System.out.println("ingresar caracteristica8");
+         String caracteristica8 = entrada.nextLine();
+         System.out.println("caracteristica 8: "+caracteristica8);
+         
+         System.out.println("ingresar caracteristica9");
+         String caracteristica9 = entrada.nextLine();
+         System.out.println("caracteristica 9: "+caracteristica9);
+         
+         System.out.println("ingresar caracteristica10");
+         String caracteristica10 = entrada.nextLine();
+         System.out.println("caracteristica 10: "+caracteristica10);
+         
+         System.out.println("ingresar caracteristica11");
+         String caracteristica11 = entrada.nextLine();
+         System.out.println("caracteristica 11: "+caracteristica11);
+         
+         System.out.println("Gracias por su atencion");
+         
+    }
+     
+}
+
+
+
+
+
+
+
